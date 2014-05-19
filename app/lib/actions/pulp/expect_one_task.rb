f0ab45583431bd733ee983f5ab000dcabc4c1efc
Justin Sherrill <jsherril@redhat.com>
2014-05-19 14:09:35 -0400

    TestFOO

---
+++ app/lib/actions/pulp/expect_one_task.rb
@@ -0,0 +1,25 @@
+#
+# Copyright 2014 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+module Actions
+  module Pulp
+    class ExpectOneTask < Actions::Base
+
+      def external_task=(external_task_data)
+        external_task_data = external_task_data unless external_task_data.is_a?(Array)
+        fail "Not expecting more than one task" if external_task_data.length  > 1
+        super(external_task_data)
+      end
+
+    end
+  end
+end
