4e3c9e9d3db842d69c1f80f6b78268c9220d35fa
Mike McCune <mmccune@redhat.com>
2018-11-08 16:12:45 -0800

    BZ #1643649 - Sequential Actions::Katello::Host::Update calls from subscription-manager can fail under load
    Fixes #25422 - optional switch to link locking for Host::Update
    
    link will queue conflicting actions and process the Host
    updates in the order they were received. Tasks will sit
    in planned:pending until the running task is completed.
    This will reduce the error rate and prevent clients from
    receiving errors because sequential API calls error
    
    (cherry picked from commit ae116f7fbd1210717368fd74af60829717f25302)

--- app/lib/actions/katello/host/update.rb
+++ app/lib/actions/katello/host/update.rb
@@ -47,7 +47,11 @@ module Actions
         end
 
         def resource_locks
-          :update
+          if Setting[:host_update_lock]
+            :link
+          else
+            :update
+          end
         end
 
         def rescue_strategy
